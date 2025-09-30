6912268561ff6a7431eaf480d22fd16884bc9805
Samuel Vergnol <46886716+Sam-Vrgl@users.noreply.github.com>
2025-09-30 16:57:36 +0200

    removing uneeded opts from test state

--- clean/js/state.js
+++ clean/js/state.js
@@ -15,17 +15,17 @@ export const state = {
   images: [
     // Example entries (url + placement opts). Start empty if you want.
     // { url: './assets/logo.png', x_mm: 20, y_mm: 5, heightPct: 0.4, anchor: 'tl', behind: false }
-    { url: '../../assets/insignes/chiffres/petit/chiffre-1-10-mm.png', x_mm: 1, y_mm: 3, heightPct: 0.7, anchor: 'tl', behind: false },
-    { url: '../../assets/insignes/chiffres/petit/chiffre-2-10-mm.png', x_mm: 30, y_mm: 3, heightPct: 0.7, anchor: 'tl', behind: false },
-    { url: '../../assets/insignes/chiffres/petit/chiffre-3-10-mm.png', x_mm: 60, y_mm: 3, heightPct: 0.7, anchor: 'tl', behind: false },
-    { url: '../../assets/insignes/chiffres/petit/chiffre-4-10-mm.png', x_mm: 90, y_mm: 3, heightPct: 0.7, anchor: 'tl', behind: false },
-    { url: '../../assets/insignes/chiffres/petit/chiffre-5-10-mm.png', x_mm: 120, y_mm: 3, heightPct: 0.7, anchor: 'tl', behind: false },
-    { url: '../../assets/insignes/chiffres/petit/chiffre-6-10-mm.png', x_mm: 150, y_mm: 3, heightPct: 0.7, anchor: 'tl', behind: false },
-    { url: '../../assets/insignes/chiffres/petit/chiffre-7-10-mm.png', x_mm: 180, y_mm: 3, heightPct: 0.7, anchor: 'tl', behind: false },
-    { url: '../../assets/insignes/chiffres/petit/chiffre-8-10-mm.png', x_mm: 210, y_mm: 3, heightPct: 0.7, anchor: 'tl', behind: false },
-    { url: '../../assets/insignes/chiffres/petit/chiffre-9-10-mm.png', x_mm: 240, y_mm: 3, heightPct: 0.7, anchor: 'tl', behind: false },
-    { url: '../../assets/insignes/chiffres/petit/chiffre-0-10-mm.png', x_mm: 270, y_mm: 3, heightPct: 0.7, anchor: 'tl', behind: false },
-    { url: '../../assets/insignes/filiere/ecole-ingenieur.png', x_mm: 314.5, y_mm: 1, heightPct: 0.9, anchor: 'tl', behind: false },
+    { url: '../../assets/insignes/chiffres/petit/chiffre-1-10-mm.png', x_mm: 1, y_mm: 3, heightPct: 0.7 },
+    { url: '../../assets/insignes/chiffres/petit/chiffre-2-10-mm.png', x_mm: 30, y_mm: 3, heightPct: 0.7 },
+    { url: '../../assets/insignes/chiffres/petit/chiffre-3-10-mm.png', x_mm: 60, y_mm: 3, heightPct: 0.7 },
+    { url: '../../assets/insignes/chiffres/petit/chiffre-4-10-mm.png', x_mm: 90, y_mm: 3, heightPct: 0.7 },
+    { url: '../../assets/insignes/chiffres/petit/chiffre-5-10-mm.png', x_mm: 120, y_mm: 3, heightPct: 0.7 },
+    { url: '../../assets/insignes/chiffres/petit/chiffre-6-10-mm.png', x_mm: 150, y_mm: 3, heightPct: 0.7 },
+    { url: '../../assets/insignes/chiffres/petit/chiffre-7-10-mm.png', x_mm: 180, y_mm: 3, heightPct: 0.7 },
+    { url: '../../assets/insignes/chiffres/petit/chiffre-8-10-mm.png', x_mm: 210, y_mm: 3, heightPct: 0.7 },
+    { url: '../../assets/insignes/chiffres/petit/chiffre-9-10-mm.png', x_mm: 240, y_mm: 3, heightPct: 0.7 },
+    { url: '../../assets/insignes/chiffres/petit/chiffre-0-10-mm.png', x_mm: 270, y_mm: 3, heightPct: 0.7 },
+    { url: '../../assets/insignes/filiere/ecole-ingenieur.png', x_mm: 314.5, y_mm: 1, heightPct: 0.9 },
   ]
 };
 
